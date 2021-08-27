e1916ac16c7cd2620b319f5d8364ded256492f9d
Micael Dos Santos <Micael Dos Santos>
2021-08-27 12:49:30 -0300

    Codigos

--- index.js
+++ index.js
@@ -1,80 +1,12 @@
-const bancoCadastrado = {
-  logo: 'Logo',
-  nome: 'Internet Bank GF',
-  agencias: [
-    {
-      numero: 1212,
-      contas: [{
-        numero: 1234,
-        senha: 4321,
-        saldo: 500,
-        usuario: {
-          nome: 'Paulo',
-          idade: 30,
-          sexo: 'M',
-          email: 'paulo@gmail.com',
-          cpf: '123.456.789-10'
-        }
-      },
-      {
-        numero: 6789,
-        senha: 9876,
-        saldo: 1000,
-        usuario: {
-          nome: 'José',
-          idade: 20,
-          sexo: 'M',
-          email: 'jose@gmail.com',
-          cpf: '123.456.789-15'
-        }
-      }
-      ]
-    },
-    {
-      numero: 1213,
-      contas: [{
-        numero: 1234,
-        senha: 4321,
-        saldo: 400,
-        usuario: {
-          nome: 'Ana',
-          idade: 80,
-          sexo: 'F',
-          email: 'ana@gmail.com',
-          cpf: '123.456.788-10'
-        }
-      }
-      ]
-    }
-  ]
-}
 
-localStorage.setItem('internetBank-GF' , JSON.stringify(bancoCadastrado))
-
+salvarDadosDoBancoNoLocalStorage();
+const bancoCadastrado = pegarDadosDoLocalStorage()
+  
 function criarConta() {
   window.location.href = "Cadastro/index.html"
 }
 
 function acessar() {
-
-  var contasComSaldoMaiorQue500 = []
-  bancoCadastrado.agencias.forEach(agencia => {
-    let contasEncontradas = agencia.contas.filter(conta => conta.saldo >= 500)
-    contasEncontradas.forEach(contaEncontrada => {
-      contasComSaldoMaiorQue500.push(contaEncontrada)
-    })
-  })
-  console.log(contasComSaldoMaiorQue500)
-
-  var sexoF = []
-  bancoCadastrado.agencias.forEach(agencia => {
-    let contasEncontradas = agencia.contas.filter( conta => conta.usuario.sexo = "F")
-    contasEncontradas.forEach(contaEncontrada => {
-      sexoF.push(contaEncontrada)
-    })
-  })
-  console.log(sexoF)
-
   const erros = verificarDados()
   if (erros.length > 0) {
     erros.forEach(erro => {
